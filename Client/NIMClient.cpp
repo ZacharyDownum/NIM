eb32ee73967dbd16d4b53eb514531ade66acc497
Zachary Downum <zdownum@gmail.com>
2019-04-27 10:16:18 -0500

    Updated NIMClient.cpp
    Changed MAX_NAME constant within nim.h to 80 (from 100) to reflect the design document for Project #5
    
    Updated NIMClient.cpp to use NIM instead of TicTacToe, and added a section for the client user to enter their name.
    
    Removed NIMClient.h as it served no purpose.

--- Client/NIMClient.cpp
+++ Client/NIMClient.cpp
@@ -1,15 +1,20 @@
 // clientMain.cpp
 //   This function serves as the "main" function for the client-side
-#include "TicTacToe.h"
+#include "nim.h"
 #include <iostream>
 #include <string>
 #include <WinSock2.h>
 
 int clientMain(int argc, char *argv[], std::string playerName)
 {
-	// Find all TicTacToe servers on our subnet
+	char clientName[MAX_NAME];
+
+	std::cout << "What is your name? ";
+	std::getline(std::cin, clientName);
+
+	// Find all NIM servers on our subnet
 	SOCKET s = connectsock("","","udp");	// Create a UDP socket  (Don't need to designate a host or port for UDP)
-	std::cout << std::endl << "Looking for TicTacToe servers ... " << std::endl;
+	std::cout << std::endl << "Looking for NIM servers ... " << std::endl;
 
 	char broadcastAddress[v4AddressSize];
 	char myIPAddress[v4AddressSize];
@@ -19,27 +24,27 @@ int clientMain(int argc, char *argv[], std::string playerName)
 		return -1;
 	}
 
-	// Build an array of TicTacToe servers found on our subnet.
+	// Build an array of NIM servers found on our subnet.
 	ServerStruct serverArray[MAX_SERVERS];
-	int numServers = getServers(s, broadcastAddress, TicTacToe_UDPPORT, serverArray);
+	int numServers = getServers(s, broadcastAddress, NIM_UDPPORT, serverArray);
 
 	if (numServers == 0) {
-		std::cout << std::endl << "Sorry.  No TicTacToe servers were found.  Try again later." << std::endl << std::endl;
+		std::cout << std::endl << "Sorry.  No NIM servers were found.  Try again later." << std::endl << std::endl;
 	} else {
-		// Display the list of TicTacToe servers found
-		std::cout << std::endl << "Found TicTacToe server";
+		// Display the list of NIM servers found
+		std::cout << std::endl << "Found NIM server";
 		if (numServers == 1) {
 			std::cout << ":" << "  " << serverArray[0].name << std::endl;
 		} else {
 			std::cout << "s:" << std::endl;
-			for (int i=0; i<numServers; i++) {
+			for (int i=0; i < numServers; i++) {
 				std::cout << "  " << i+1 << " - " << serverArray[i].name << std::endl;
 			}
 			std::cout << std::endl << "  " << numServers+1 << " - QUIT" << std::endl;
 		}
 		std::cout << std::endl;
 
-		// Allow user to select a TicTacToe server to challenge to a game
+		// Allow user to select a NIM server to challenge to a game
 		int answer = 0;
 		std::string answerStr;
 		if (numServers == 1) {
@@ -62,17 +67,17 @@ int clientMain(int argc, char *argv[], std::string playerName)
 			strcpy_s(host,serverArray[answer-1].host.c_str());
 			strcpy_s(port,serverArray[answer-1].port.c_str());
 
-			// Append playerName to the TicTacToe_CHALLENGE string & send a challenge to host:port
+			// Append playerName to the NIM_CHALLENGE string & send a challenge to host:port
 			char buffer[MAX_SEND_BUFFER];
-			strcpy_s(buffer,TicTacToe_CHALLENGE);
-			strcat_s(buffer,playerName.c_str());
+			strcpy_s(buffer, NIM_CHALLENGE);
+			strcat_s(buffer, playerName.c_str());
 			int len = UDP_send(s, buffer, strlen(buffer)+1, host, port);
 			if (debug) {
 				std::cout << timestamp() << " - Sent: " << buffer << " to " << host << ":" << port << std::endl;
 			}
 
 			// Play the game.  You are the 'X' player
-			int winner = playTicTacToe(s, serverName, host, port, PLAYER_X);
+			int winner = playNIM(s, serverName, host, port, PLAYER_X);
 		}
 	}
 
